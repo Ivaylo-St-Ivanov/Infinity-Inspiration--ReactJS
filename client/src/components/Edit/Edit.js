b28653476f7a2ca7b2f8b8b66a8749cd1fc6d639
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-03-30 08:57:48 +0300

    Change edit-form and update CSS file.

--- client/src/components/Edit/Edit.js
+++ client/src/components/Edit/Edit.js
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useForm } from '../../hooks/useForm';
+// import { useForm } from '../../hooks/useForm';
 import { StateContext } from '../../contexts/StateContext';
 import * as postService from '../../services/postsService';
 
@@ -10,22 +10,29 @@ import styles from '../CreatePost/CreatePost.module.css';
 export const Edit = () => {
     const { onEditPostSubmit } = useContext(StateContext);
 
-    const [initValues, setValues] = useState([]);
+    const [initValues, setValues] = useState({});
     const { postId } = useParams();
-    const { values, changeHandler, onSubmit } = useForm({
-        _id: '',
-        title: '',
-        imageUrl: '',
-        author: '',
-        content: ''
-    }, onEditPostSubmit);
-    // console.log(initValues._id);
+
     useEffect(() => {
         postService.getOnePost(postId)
             .then(result => {
                 setValues(result);
             });
     }, [postId]);
+    console.log(initValues._id);
+    // const { values, changeHandler, onSubmit } = useForm({
+    //     _id: initValues._id,
+    //     title: initValues.title,
+    //     imageUrl: initValues.imageUrl,
+    //     author: initValues.author,
+    //     content: initValues.content
+    // }, onEditPostSubmit);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        onEditPostSubmit(initValues);
+    };
 
     return (
         <main className={styles['edit']}>
@@ -38,31 +45,35 @@ export const Edit = () => {
                         name="title"
                         id="post-title"
                         placeholder="Title"
-                        value={values.title}
-                        onChange={changeHandler}
+                        // value={values.title}
+                        // onChange={changeHandler}
+                        defaultValue={initValues.title}
                     />
                     <input
                         type="text"
                         name="imageUrl"
                         id="img-url"
                         placeholder="Image url"
-                        value={values.imageUrl}
-                        onChange={changeHandler}
+                        // value={values.imageUrl}
+                        // onChange={changeHandler}
+                        defaultValue={initValues.imageUrl}
                     />
                     <input
                         type="text"
                         name="author"
                         id="post-author"
                         placeholder="Author"
-                        value={values.author}
-                        onChange={changeHandler}
+                        // value={values.author}
+                        // onChange={changeHandler}
+                        defaultValue={initValues.author}
                     />
                     <textarea
                         id="post-content"
                         name="content"
                         placeholder="Content"
-                        value={values.content}
-                        onChange={changeHandler}
+                        // value={values.content}
+                        // onChange={changeHandler}
+                        defaultValue={initValues.content}
                         rows="12"
                     ></textarea>
 
@@ -73,7 +84,7 @@ export const Edit = () => {
             </div>
 
             <div className={styles['img']}>
-                <img src="./images/chess.avif" alt="chess" />
+                <img src="/images/chess.avif" alt="chess" />
             </div>
         </main>
     );
