641a8a98b05264214bf189be48bc88af5a6345a9
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-04-11 11:29:53 +0300

    Update

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,19 +1,24 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import { CatalogItem } from './CatalogItem/CatalogItem';
-
-import styles from './Catalog.module.css';
 import { StateContext } from '../../contexts/StateContext';
+import { UserContext } from '../../contexts/UserContext';
+
+import { CatalogItem } from './CatalogItem/CatalogItem';
+import styles from './Catalog.module.css';
 
 export const Catalog = () => {
     const { posts } = useContext(StateContext);
+    const { isAuthenticated } = useContext(UserContext);
     return (
         <div className={styles['main-catalog']}>
             <section id={styles['facts']}>
-                <div>
+                
+                {isAuthenticated && (
+                    <div>
                     <Link className="btn btn-create" to={'/create-post'}>Create Post</Link>
                 </div>
+                )}
 
                 <div className={styles['cards']}>
 
